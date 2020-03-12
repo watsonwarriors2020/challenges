4f66f064b1a27ab376d3575a271e73bcaf69cf18
Alejandro Esquivel <alejandroesquivel@Alejandros-MacBook-Pro.local>
2020-03-12 16:48:21 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
-echo "test"
+
+repo="https://raw.githubusercontent.com/watsonwarriors2020/challenges/master/"
+wget --quiet $repo"ww.txt"
+
+files=( "ww.py" "Challenge%201%20-%20Enter%20Watson.ipynb" )
+
+cat './ww.txt'
+echo "\nFetching Watson Warriors dependencies...\n"
+
+for file in "${files[@]}"
+do
+    wget -N --quiet --show-progress $repo$file
+done
