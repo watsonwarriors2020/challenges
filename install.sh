3534a2b496139631f1d7d4e627b52a631350edcf
Alejandro Esquivel <alejandroesquivel@Alejandros-MacBook-Pro.local>
2020-03-12 20:00:02 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ wget --quiet $repo"ww.txt"
 files=( "ww.py" "Challenge%201%20-%20Enter%20Watson.ipynb" )
 
 cat ./ww.txt
-echo ""
-echo "\nFetching Watson Warriors dependencies...\n"
+
+printf "\nFetching Watson Warriors dependencies...\n"
 
 for file in "${files[@]}"
 do
     wget -N --quiet --show-progress $repo$file
 done
 
-echo "\nFinished. Please return to the Watson Warriors dashboard and proceed with the current challenge. \n"
+printf "\nFinished. Please return to the Watson Warriors dashboard and proceed with the current challenge. \n"
