6ce7a7744fe9b146cb77c3c40b73ed9f879d0e00
Alejandro Esquivel <alejandroesquivel@Alejandros-MacBook-Pro.local>
2020-03-15 14:45:43 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 repo="https://raw.githubusercontent.com/watsonwarriors2020/challenges/master/"
 wget --quiet $repo"ww.txt"
 
-files=( "ww.py" "Challenge%209%20-%20Step%20it%20UP%20(py)%20v2.ipynb" "Challenge%207%20Fight%20Fire%20with%20Data%20(PY)%20v2.ipynb" "Challenge%206%20-%20Where%20There%20is%20Smoke%20TF%20Ke.ipynb" "fire_weather_speed_dis.csv" )
+files=( "ww.py" "Challenge%209%20-%20Step%20it%20UP%20(py)%20v2.ipynb" "Challenge%207%20Fight%20Fire%20with%20Data%20(PY)%20v2.ipynb" "Challenge%206%20-%20Where%20There%20is%20Smoke%20TF%20Ke.ipynb" "fire_weather_speed_dis.csv" "Challenge_7_Merged_Data_single_fire.csv" )
 
 cat ./ww.txt
 
